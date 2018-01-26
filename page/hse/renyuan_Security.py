f69f800f028e2d3c67fc81d41bf37a550e9fc56b
shishuaigang <shishuaigang@163.com>
2018-01-26 17:18:54 +0800

    xpath使用"=",用于更精确的定位

--- page/hse/renyuan_Security.py
+++ page/hse/renyuan_Security.py
@@ -1,3 +1,3 @@
 from appium.webdriver.common.mobileby import By
 
-zidingyi_search = (By.XPATH, "//android.widget.TextView[contains(@text,'自定义查询')]")
+zidingyi_search = (By.XPATH, "//android.widget.TextView[@text='自定义查询']")
--- testcase/try_testcase.py
+++ testcase/try_testcase.py
@@ -10,8 +10,21 @@ def ceshi(d):
     d.find_element(*shouye.HSE).click()
     d.find_element(*HSE.renyuan_security).click()
     d.find_element(*renyuan_Security.zidingyi_search).click()
+    d.find_element_by_id("com.gongzhidao.inroad:id/edit_begindate").send_keys("123456")
 
 
 if __name__ == "__main__":
     d = Driver().get_android_driver()
     ceshi(d)
+'''
+
+
+if __name__ == "__main__":
+    d = Driver().get_android_driver()
+    mesg = '//*[@text=\'{}\']'.format("网络连接错误")
+    el = WebDriverWait(d, 20, 0.5).until(ec.presence_of_all_elements_located((By.XPATH, mesg)))
+    if el:
+        print(1)
+    else:
+        print(2)
+'''
